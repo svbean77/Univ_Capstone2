37b005cc5dd74bc0127cee9793851a92dd691ecd
DESKTOP-ANUKI9H\svbea <svbean77@naver.com>
2022-10-19 13:35:32 +0900

    divide muscle

--- final_app/lib/exercise/select_body.dart
+++ final_app/lib/exercise/select_body.dart
@@ -192,56 +192,70 @@ class SelectBody extends StatelessWidget {
     switch (type) {
       case 'f':
         if (col == 1 || col == 9) {
-          muscleLst[0] = 1;
-          muscleLst[1] = 1;
-          muscleLst[2] = 1;
-          muscleLst[3] = 1;
+          muscleLst[6] = 10;
+        } else if (col == 2 || col == 8) {
+          muscleLst[4] = 1;
+          muscleLst[5] = 10;
+          muscleLst[6] = 10;
+        } else if (col == 3 || col == 7) {
+          muscleLst[3] = 6;
           muscleLst[4] = 1;
           muscleLst[5] = 1;
-          muscleLst[6] = 1;
-          muscleLst[7] = 1;
-          muscleLst[8] = 1;
-        } else if (col == 2 || col == 8) {
-          muscleLst[0] = 1;
-          muscleLst[1] = 1;
-          muscleLst[2] = 1;
-          muscleLst[3] = 1;
-          muscleLst[4] = 1;
-        } else if (col == 3 || col == 7) {
+          muscleLst[12] = 11;
+          muscleLst[13] = 11;
+        } else if (col == 4 || col == 6) {
+          muscleLst[2] = 4;
           muscleLst[3] = 2;
           muscleLst[4] = 2;
-          muscleLst[5] = 2;
-          muscleLst[6] = 2;
-          muscleLst[7] = 2;
-          muscleLst[8] = 2;
-        } else if (col == 4 || col == 6) {
-          muscleLst[0] = 1;
-          muscleLst[1] = 1;
-          muscleLst[2] = 1;
-          muscleLst[3] = 1;
-          muscleLst[4] = 1;
+          muscleLst[5] = 15;
+          muscleLst[6] = 15;
+          muscleLst[8] = 3;
+          muscleLst[9] = 3;
+          muscleLst[10] = 3;
+          muscleLst[12] = 11;
+          muscleLst[13] = 11;
         } else {
           muscleLst[3] = 2;
           muscleLst[4] = 2;
-          muscleLst[5] = 2;
-          muscleLst[6] = 2;
-          muscleLst[7] = 2;
-          muscleLst[8] = 2;
+          muscleLst[5] = 12;
+          muscleLst[6] = 12;
+          muscleLst[7] = 12;
         }
         break;
       case 'b':
         if (col == 1 || col == 9) {
-          muscleLst[6] = 1;
+          muscleLst[6] = 10;
         } else if (col == 2 || col == 8) {
-          muscleLst[6] = 1;
+          muscleLst[4] = 5;
+          muscleLst[5] = 10;
+          muscleLst[6] = 10;
         } else if (col == 3 || col == 7) {
-          muscleLst[3] = 2;
-        } else if (col == 4 || col == 6) {
-          muscleLst[2] = 3;
-          muscleLst[3] = 4;
-        } else {
-          muscleLst[3] = 4;
+          muscleLst[3] = 6;
+          muscleLst[4] = 5;
           muscleLst[5] = 5;
+          muscleLst[12] = 11;
+          muscleLst[13] = 11;
+        } else if (col == 4 || col == 6) {
+          muscleLst[2] = 4;
+          muscleLst[3] = 6;
+          muscleLst[4] = 7;
+          muscleLst[5] = 7;
+          muscleLst[6] = 7;
+          muscleLst[7] = 9;
+          muscleLst[8] = 9;
+          muscleLst[9] = 8;
+          muscleLst[10] = 8;
+          muscleLst[12] = 11;
+          muscleLst[13] = 11;
+        } else {
+          muscleLst[2] = 4;
+          muscleLst[3] = 14;
+          muscleLst[4] = 14;
+          muscleLst[5] = 13;
+          muscleLst[6] = 13;
+          muscleLst[7] = 9;
+          muscleLst[8] = 9;
+          muscleLst[9] = 8;
         }
         break;
     }
@@ -299,38 +313,21 @@ class EachCols extends StatelessWidget {
         for (int i = 1; i <= 16; i++)
           LoadImage(
               onTap: () {
-                switch (muscleLst[i]) {
-                  case 0:
-                    break;
-                  case 1:
-                    Navigator.of(context).push(
-                      MaterialPageRoute(
-                        builder: (BuildContext context) => ExerciseGuide(
-                          muscleName: exerciseLst[1][0],
-                          exerciseName: exerciseLst[1][1],
-                          exerciseImage: exerciseLst[1][2],
-                          equipment: exerciseLst[1][3],
-                          difficulty: exerciseLst[1][4],
-                          exerciseStep: exerciseLst[1][5],
-                        ),
+                if (muscleLst[i-1] == 0)
+                  Navigator.of(context).noSuchMethod;
+                else
+                  Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (BuildContext context) => ExerciseGuide(
+                        muscleName: exerciseLst[muscleLst[i-1] - 1][0],
+                        exerciseName: exerciseLst[muscleLst[i-1] - 1][1],
+                        exerciseImage: exerciseLst[muscleLst[i-1] - 1][2],
+                        equipment: exerciseLst[muscleLst[i-1] - 1][3],
+                        difficulty: exerciseLst[muscleLst[i-1] - 1][4],
+                        exerciseStep: exerciseLst[muscleLst[i-1] - 1][5],
                       ),
-                    );
-                    break;
-                  case 2:
-                    Navigator.of(context).push(
-                      MaterialPageRoute(
-                        builder: (BuildContext context) => ExerciseGuide(
-                          muscleName: exerciseLst[2][0],
-                          exerciseName: exerciseLst[2][1],
-                          exerciseImage: exerciseLst[2][2],
-                          equipment: exerciseLst[2][3],
-                          difficulty: exerciseLst[2][4],
-                          exerciseStep: exerciseLst[2][5],
-                        ),
-                      ),
-                    );
-                    break;
-                }
+                    ),
+                  );
               },
               dir: dir,
               col: col,
@@ -348,43 +345,141 @@ class EachCols extends StatelessWidget {
     String exerciseStep = '''''';
 
     switch (num) {
-      case 0:
-        break;
       case 1:
-        muscleName = "근육 1";
-        exerciseName = "운동 1";
+        muscleName = "Biceps";
+        exerciseName = "이두근인가";
         exerciseImage =
-        "https://www.musclewiki.com/media/uploads/male-barbell-curl-front_uKPCb8P.gif";
+            "https://www.musclewiki.com/media/uploads/male-dumbbell-hammercurl-front.gif";
         equipment = '기구 1';
         difficulty = '난이도 1';
         exerciseStep = '''운동 방법 1''';
         break;
       case 2:
-        muscleName = "근육 2";
-        exerciseName = "운동 2";
+        muscleName = "Chest";
+        exerciseName = "가슴운동";
         exerciseImage =
-        "https://www.musclewiki.com/media/uploads/male-dumbbell-hammercurl-front.gif";
+            "https://www.musclewiki.com/media/uploads/male-dumbbell-chest-flyes-front.gif";
         equipment = '기구 2';
         difficulty = '난이도 2';
         exerciseStep = '''운동 방법 2''';
         break;
       case 3:
-        muscleName = "근육 3";
-        exerciseName = "운동 3";
+        muscleName = "Quads";
+        exerciseName = "허벅지";
         exerciseImage =
-        "https://www.musclewiki.com/media/uploads/male-barbell-curl-front_uKPCb8P.gif";
+            "https://www.musclewiki.com/media/uploads/male-dumbbell-goblet-squat-front.gif";
         equipment = '기구 3';
         difficulty = '난이도 3';
         exerciseStep = '''운동 방법 3''';
         break;
-      default:
-        muscleName = "근육 4";
-        exerciseName = "운동 4";
+      case 4:
+        muscleName = "Traps";
+        exerciseName = "승모근";
         exerciseImage =
-        "https://www.musclewiki.com/media/uploads/male-dumbbell-hammercurl-front.gif";
-        equipment = '기구 4';
-        difficulty = '난이도 4';
-        exerciseStep = '''운동 방법 4''';
+            "https://www.musclewiki.com/media/uploads/kettlebell-male-uprightrow-side.gif";
+        equipment = '기구 3';
+        difficulty = '난이도 3';
+        exerciseStep = '''운동 방법 3''';
+        break;
+      case 5:
+        muscleName = "Triceps";
+        exerciseName = "삼두근인가";
+        exerciseImage =
+            "https://www.musclewiki.com/media/uploads/male-bench-tricep-dip-front.gif";
+        equipment = '기구 3';
+        difficulty = '난이도 3';
+        exerciseStep = '''운동 방법 3''';
+        break;
+      case 6:
+        muscleName = "Shoulders";
+        exerciseName = "어깨";
+        exerciseImage =
+            "https://www.musclewiki.com/media/uploads/male-dumbbell-seated-shoulder-press-front.gif";
+        equipment = '기구 3';
+        difficulty = '난이도 3';
+        exerciseStep = '''운동 방법 3''';
+        break;
+      case 7:
+        muscleName = "Lats";
+        exerciseName = "옆구리등쪽";
+        exerciseImage =
+            "https://www.musclewiki.com/media/uploads/male-dumbbell-bilateral-row-front.gif";
+        equipment = '기구 3';
+        difficulty = '난이도 3';
+        exerciseStep = '''운동 방법 3''';
+        break;
+      case 8:
+        muscleName = "Hamstrings";
+        exerciseName = "햄스트링";
+        exerciseImage =
+            "https://www.musclewiki.com/media/uploads/ProneLegCurl-Back-021316.gif";
+        equipment = '기구 3';
+        difficulty = '난이도 3';
+        exerciseStep = '''운동 방법 3''';
+        break;
+      case 9:
+        muscleName = "Glutes";
+        exerciseName = "엉덩이";
+        exerciseImage =
+            "https://www.musclewiki.com/media/uploads/bodyweight-slglutebridge-male-side.gif";
+        equipment = '기구 3';
+        difficulty = '난이도 3';
+        exerciseStep = '''운동 방법 3''';
+        break;
+      case 10:
+        muscleName = "Forearms";
+        exerciseName = "팔";
+        exerciseImage =
+            "https://www.musclewiki.com/media/uploads/male-db-wristflex-front.gif";
+        equipment = '기구 3';
+        difficulty = '난이도 3';
+        exerciseStep = '''운동 방법 3''';
+        break;
+      case 11:
+        muscleName = "Calves";
+        exerciseName = "종아리";
+        exerciseImage =
+            "https://www.musclewiki.com/media/uploads/kettlebell-male-sl-calf-raises-front.gif";
+        equipment = '기구 3';
+        difficulty = '난이도 3';
+        exerciseStep = '''운동 방법 3''';
+        break;
+      case 12:
+        muscleName = "Abdominals";
+        exerciseName = "복근";
+        exerciseImage =
+            "https://www.musclewiki.com/media/uploads/female-leg-raises-front.gif";
+        equipment = '기구 3';
+        difficulty = '난이도 3';
+        exerciseStep = '''운동 방법 3''';
+        break;
+      case 13:
+        muscleName = "Lower back";
+        exerciseName = "등 아래";
+        exerciseImage =
+            "https://www.musclewiki.com/media/uploads/kettlebell-male-deadlift-front_Omfp4bo.gif";
+        equipment = '기구 3';
+        difficulty = '난이도 3';
+        exerciseStep = '''운동 방법 3''';
+        break;
+      case 14:
+        muscleName = "Traps (mid-back)";
+        exerciseName = "등 중간";
+        exerciseImage =
+            "https://www.musclewiki.com/media/uploads/male-dumbbell-bilateral-row-side.gif";
+        equipment = '기구 3';
+        difficulty = '난이도 3';
+        exerciseStep = '''운동 방법 3''';
+        break;
+      case 15:
+        muscleName = "Obliques";
+        exerciseName = "옆구리";
+        exerciseImage =
+            "https://www.musclewiki.com/media/uploads/kettlebell-male-woodchopper-side.gif";
+        equipment = '기구 3';
+        difficulty = '난이도 3';
+        exerciseStep = '''운동 방법 3''';
+        break;
     }
     return [
       muscleName,
