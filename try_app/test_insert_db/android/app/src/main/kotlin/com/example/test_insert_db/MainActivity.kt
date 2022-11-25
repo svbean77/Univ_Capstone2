7385b5af4ee85371b00c4e1f177fbeea8172f1ec
DESKTOP-ANUKI9H\svbea <svbean77@naver.com>
2022-11-25 10:51:44 +0900

    test insert into db

---
+++ try_app/test_insert_db/android/app/src/main/kotlin/com/example/test_insert_db/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.test_insert_db
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ try_app/test_insert_db/lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:test_insert_db/screen/home_screen.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: HomeScreen(),
+    ),
+  );
+}
