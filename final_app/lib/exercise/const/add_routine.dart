895bd9a2697a28193e801169aef7ba09ed7e0989
DESKTOP-ANUKI9H\svbea <svbean77@naver.com>
2022-11-28 15:12:21 +0900

    add routine at my routine

--- final_app/lib/exercise/const/add_routine.dart
+++ final_app/lib/exercise/const/add_routine.dart
@@ -61,10 +61,10 @@ class _AddRoutineState extends State<AddRoutine> {
                   '취소',
                   style: TextStyle(
                     color: (widget.grade == 0 ||
-                        widget.grade == 1 ||
-                        widget.grade == 2 ||
-                        widget.grade == 4 ||
-                        widget.grade == 8)
+                            widget.grade == 1 ||
+                            widget.grade == 2 ||
+                            widget.grade == 4 ||
+                            widget.grade == 8)
                         ? Colors.black
                         : Colors.white,
                   ),
@@ -75,25 +75,34 @@ class _AddRoutineState extends State<AddRoutine> {
                   primary: PRIMARY_COLOR[widget.grade],
                   elevation: 0,
                 ),
-                onPressed: () {
+                onPressed: () async {
+                  var url = Uri.http(
+                      IP_ADDRESS, '/test_add_routine.php', {'q': '{http}'});
+                  var response = await http.post(url, body: <String, String>{
+                    "nickname": widget.loginID.toString(),
+                    "routineName": _controller.text.toString(),
+                  });
+                  var jsondata =
+                      jsonDecode(json.decode(json.encode(response.body)));
                   /*
                       insert: loginID가 만든 루틴이름 만들기 (insert)
                        */
-                  Navigator.of(context).pushReplacement(
-                    MaterialPageRoute(
-                      builder: (BuildContext context) => SelectMyRoutine(
-                          loginID: widget.loginID, grade: widget.grade),
-                    ),
-                  );
+                  if (jsondata == "Success")
+                    Navigator.of(context).pushReplacement(
+                      MaterialPageRoute(
+                        builder: (BuildContext context) => SelectMyRoutine(
+                            loginID: widget.loginID, grade: widget.grade),
+                      ),
+                    );
                 },
                 child: Text(
                   '확인',
                   style: TextStyle(
                     color: (widget.grade == 0 ||
-                        widget.grade == 1 ||
-                        widget.grade == 2 ||
-                        widget.grade == 4 ||
-                        widget.grade == 8)
+                            widget.grade == 1 ||
+                            widget.grade == 2 ||
+                            widget.grade == 4 ||
+                            widget.grade == 8)
                         ? Colors.black
                         : Colors.white,
                   ),
