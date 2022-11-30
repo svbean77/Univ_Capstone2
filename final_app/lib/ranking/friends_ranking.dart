2f70a1a899b0dee2a9269555aefe7190b9449179
DESKTOP-ANUKI9H\svbea <svbean77@naver.com>
2022-11-30 23:56:41 +0900

    add friends ranking

--- final_app/lib/ranking/friends_ranking.dart
+++ final_app/lib/ranking/friends_ranking.dart
@@ -1,7 +1,12 @@
+import 'dart:convert';
+
 import 'package:final_app/ranking/const/ranking_card.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import '../screen/const/db_class.dart';
+import '../screen/const/ip_address.dart';
 
-class FriendsRanking extends StatelessWidget {
+class FriendsRanking extends StatefulWidget {
   final loginID;
   final grade;
   const FriendsRanking({
@@ -9,6 +14,22 @@ class FriendsRanking extends StatelessWidget {
     required this.grade,
     Key? key,
   }) : super(key: key);
+
+  @override
+  State<FriendsRanking> createState() => _FriendsRankingState();
+}
+
+class _FriendsRankingState extends State<FriendsRanking> {
+  Future getDatas() async {
+    var url =
+        Uri.http(IP_ADDRESS, '/test_select_all_friends.php', {'q': '{http}'});
+    var response = await http.post(url, body: <String, String>{
+      "nickname": widget.loginID.toString(),
+    });
+    var jsondata = jsonDecode(json.decode(json.encode(response.body)));
+    USERDATA data = USERDATA.fromJson(jsondata);
+    return data;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -19,24 +40,36 @@ class FriendsRanking extends StatelessWidget {
     List<String> rating = ['챌린저', '다이아몬드', '골드', '브론즈', '아이언'];
     List<int> total3th = [500, 380, 220, 190, 80];
 
-    return Padding(
-      padding: const EdgeInsets.all(16.0),
-      child: Container(
-        child: ListView.separated(
-            itemBuilder: (context, index) {
-              return RankingCard(
-                  grade: grade,
-                  nickname: nickname[index],
-                  rating: rating[index],
-                  ranking: index + 1,
-                  total3th: total3th[index],
-                  loginID: loginID);
-            },
-            separatorBuilder: (context, index) {
-              return SizedBox(height: 8.0);
-            },
-            itemCount: nickname.length),
-      ),
-    );
+    return FutureBuilder(
+        future: getDatas(),
+        builder: (context, snapshot) {
+          return Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Container(
+              child: ListView(
+                children: [
+                  if (snapshot.hasData)
+                    if (snapshot.data.result!.length != 0)
+                      for (int i = 0; i < snapshot.data.result!.length; i++)
+                        Padding(
+                          padding: const EdgeInsets.only(bottom: 8.0),
+                          child: RankingCard(
+                            grade: widget.grade,
+                            loginID: widget.loginID,
+                            nickname: snapshot.data.result![i].nickname,
+                            rating: snapshot.data.result![i].rating,
+                            ranking: i + 1,
+                            total3th: snapshot.data.result![i].total,
+                          ),
+                        )
+                    else
+                      Center(child: Text('친구 목록 없습니다.'))
+                  else
+                    Center(child: CircularProgressIndicator())
+                ],
+              ),
+            ),
+          );
+        });
   }
 }
