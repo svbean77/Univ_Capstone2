529ec31d9930b107318c2efe5b6bbf2b88d79f26
DESKTOP-ANUKI9H\svbea <svbean77@naver.com>
2022-12-01 17:42:25 +0900

    success reqeust friends

--- final_app/lib/ranking/const/my_ranking.dart
+++ final_app/lib/ranking/const/my_ranking.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'dart:convert';
 
 import 'package:final_app/ranking/const/user_info.dart';
@@ -20,6 +21,9 @@ class MyRanking extends StatefulWidget {
 }
 
 class _MyRankingState extends State<MyRanking> {
+  StreamController controller = StreamController();
+  Timer? _timer;
+
   Future getDatas() async {
     int ranking = 0;
     List<String> friendsLst = [];
@@ -42,7 +46,7 @@ class _MyRankingState extends State<MyRanking> {
     var jsondata2 = jsonDecode(json.decode(json.encode(response2.body)));
     USERDATA data2 = USERDATA.fromJson(jsondata2);
 
-    //내가 한 요청: 대기중 아이콘을 보임
+    //내가 받은 요청: 수락 아이콘을 보임
     var url3 =
         Uri.http(IP_ADDRESS, '/test_select_requested.php', {'q': '{http}'});
     var response3 = await http.post(url3, body: <String, String>{
@@ -51,7 +55,7 @@ class _MyRankingState extends State<MyRanking> {
     var jsondata3 = jsonDecode(json.decode(json.encode(response3.body)));
     REQUESTED data3 = REQUESTED.fromJson(jsondata3);
 
-    //내가 받은 요청: 수락 아이콘을 보임
+    //내가 한 요청: 대기중 아이콘을 보임
     var url4 =
         Uri.http(IP_ADDRESS, '/test_select_request.php', {'q': '{http}'});
     var response4 = await http.post(url4, body: <String, String>{
@@ -60,9 +64,8 @@ class _MyRankingState extends State<MyRanking> {
     var jsondata4 = jsonDecode(json.decode(json.encode(response4.body)));
     REQUESTED data4 = REQUESTED.fromJson(jsondata4);
 
-    for(int i = 0; i < data.result!.length; i++){
-      if (data.result![i].nickname == widget.nickname)
-        ranking = i;
+    for (int i = 0; i < data.result!.length; i++) {
+      if (data.result![i].nickname == widget.nickname) ranking = i;
     }
     for (int i = 0; i < data2.result!.length; i++) {
       friendsLst.add(data2.result![i].nickname!);
@@ -73,46 +76,46 @@ class _MyRankingState extends State<MyRanking> {
     for (int i = 0; i < data4.result!.length; i++) {
       requestLst.add(data4.result![i].request!);
     }
-    return [data, data2, data3, data4];
+
+    controller.add([friendsLst, requestedLst, requestLst, ranking, data]);
+  }
+
+  @override
+  void initState() {
+    getDatas();
+    _timer = Timer.periodic(Duration(seconds: 1), (timer) => getDatas());
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    if (_timer!.isActive) _timer!.cancel();
+    super.dispose();
   }
 
   String friends = 'no';
+
   @override
   Widget build(BuildContext context) {
     /*
     조건문 순서: 나야 -> 요청받았어 -> 요청했어 -> 친구가 아니야 -> 친구야
      */
-    return FutureBuilder(
-        future: getDatas(),
-        builder: (context, snapshot) {
-          int ranking = 0;
-          List<String> friendsLst = [];
-          List<String> requestLst = [];
-          List<String> requestedLst = [];
-
+    return StreamBuilder(
+        stream: controller.stream,
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
           if (snapshot.hasData) {
-            for (int i = 0; i < snapshot.data[0].result!.length; i++) {
-              if (snapshot.data[0].result![i].nickname == widget.nickname)
-                ranking = i;
-            }
-            for (int i = 0; i < snapshot.data[1].result!.length; i++) {
-              friendsLst.add(snapshot.data[1].result![i].nickname);
-            }
-            for (int i = 0; i < snapshot.data[2].result!.length; i++) {
-              requestedLst.add(snapshot.data[2].result![i].request);
-            }
-            for (int i = 0; i < snapshot.data[3].result!.length; i++) {
-              requestLst.add(snapshot.data[3].result![i].request);
-            }
-
-            if (widget.loginID == widget.nickname)
-              friends = "me";
-            else if (friendsLst.contains(widget.nickname))
+            print(snapshot.data[0]);
+            print(snapshot.data[1]);
+            print(snapshot.data[2]);
+            print(snapshot.data[3]);
+            if (snapshot.data[0].contains(widget.nickname))
               friends = "friend";
-            else if (requestedLst.contains(widget.nickname))
+            else if (snapshot.data[1].contains(widget.nickname))
               friends = "requested";
-            else if (requestLst.contains(widget.nickname)) friends = "request";
+            else if (snapshot.data[2].contains(widget.nickname))
+              friends = "request";
           }
+
           print(friends);
           return Container(
             height: 200.0,
@@ -124,10 +127,9 @@ class _MyRankingState extends State<MyRanking> {
                         mainAxisAlignment: MainAxisAlignment.center,
                         children: [
                           Image.asset(
-                              'asset/images/ranking/${snapshot.data[0].result![ranking].rating}.png',
+                              'asset/images/ranking/${snapshot.data[4].result![snapshot.data[3]].rating}.png',
                               height: 150.0),
-                          Text(
-                              '${snapshot.data[0].result![ranking].rating} (${ranking + 1}위)'),
+                          Text('${snapshot.data[3] + 1}위'),
                         ],
                       ),
                       Column(
@@ -144,12 +146,16 @@ class _MyRankingState extends State<MyRanking> {
                               //친구가 아님
                               friends == 'no'
                                   ? GestureDetector(
-                                      onTap: () {
-                                        /*
-                                insert: 친구요청 (내가 친구를 요청함)
-                                 */
-                                        setState(() {
-                                          friends = 'request';
+                                      onTap: () async {
+                                        var url = Uri.http(
+                                            IP_ADDRESS,
+                                            '/test_add_request.php',
+                                            {'q': '{http}'});
+                                        var response = await http
+                                            .post(url, body: <String, String>{
+                                          "nickname": widget.loginID.toString(),
+                                          "requested":
+                                              widget.nickname.toString(),
                                         });
                                       },
                                       child: Icon(Icons.person_add, size: 25.0),
@@ -184,15 +190,15 @@ class _MyRankingState extends State<MyRanking> {
                           ),
                           SizedBox(height: 8.0),
                           Text(
-                              '3대 총합: ${snapshot.data[0].result![ranking].total}kg',
+                              '3대 총합: ${snapshot.data[4].result![snapshot.data[3]].total}kg',
                               style: TextStyle(
                                   fontSize: 20.0, fontWeight: FontWeight.bold)),
                           Text(
-                              '데드리프트: ${snapshot.data[0].result![ranking].deadlift}kg'),
+                              '데드리프트: ${snapshot.data[4].result![snapshot.data[3]].deadlift}kg'),
                           Text(
-                              '벤치프레스: ${snapshot.data[0].result![ranking].benchpress}kg'),
+                              '벤치프레스: ${snapshot.data[4].result![snapshot.data[3]].benchpress}kg'),
                           Text(
-                              '스쿼트: ${snapshot.data[0].result![ranking].squat}kg'),
+                              '스쿼트: ${snapshot.data[4].result![snapshot.data[3]].squat}kg'),
                         ],
                       ),
                     ],
