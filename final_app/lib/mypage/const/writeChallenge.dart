86b87092a394bf1b1132bc4a7f35ffc22b090c83
DESKTOP-ANUKI9H\svbea <svbean77@naver.com>
2022-12-02 01:14:39 +0900

    insert video in db

--- final_app/lib/mypage/const/writeChallenge.dart
+++ final_app/lib/mypage/const/writeChallenge.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:final_app/mypage/const/challenge_main.dart';
 import 'package:final_app/screen/const/app_bar.dart';
 import 'package:final_app/screen/const/drawer.dart';
@@ -5,6 +7,8 @@ import 'package:final_app/screen/const/grade_colors.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'dart:io';
+import 'package:http/http.dart' as http;
+import '../../screen/const/ip_address.dart';
 
 class WriteChallenge extends StatefulWidget {
   final loginID;
@@ -23,7 +27,8 @@ class WriteChallenge extends StatefulWidget {
 
 class _WriteChallengeState extends State<WriteChallenge> {
   List<File> files = [];
-  final TextEditingController contentsController = TextEditingController();
+  TextEditingController titleController = TextEditingController();
+  TextEditingController contentsController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -33,12 +38,42 @@ class _WriteChallengeState extends State<WriteChallenge> {
         padding: EdgeInsets.all(16.0),
         child: ListView(
           children: [
+            Container(
+              height: 50.0,
+              width: double.infinity,
+              child: Row(
+                children: [
+                  Text('제목', style: TextStyle(fontSize: 20.0)),
+                  SizedBox(width: 16.0),
+                  Expanded(
+                    child: Container(
+                      padding: EdgeInsets.only(left: 8.0),
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          color: widget.grade == 0
+                              ? Colors.grey.withOpacity(0.2)
+                              : PRIMARY_COLOR[widget.grade],
+                        ),
+                      ),
+                      child: TextFormField(
+                        controller: titleController,
+                        decoration: InputDecoration(
+                          border: InputBorder.none,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 16.0),
             Text('내용', style: TextStyle(fontSize: 20.0)),
             SizedBox(height: 8.0),
             Container(
               height: 300.0,
               width: double.infinity,
               child: Container(
+                padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 5.0),
                 decoration: BoxDecoration(
                   border: Border.all(
                     color: widget.grade == 0
@@ -46,10 +81,9 @@ class _WriteChallengeState extends State<WriteChallenge> {
                         : PRIMARY_COLOR[widget.grade],
                   ),
                 ),
-                child: TextField(
+                child: TextFormField(
                   controller: contentsController,
                   decoration: InputDecoration(
-                    contentPadding: EdgeInsets.symmetric(horizontal: 10.0),
                     border: InputBorder.none,
                   ),
                   maxLines: 15,
@@ -59,21 +93,22 @@ class _WriteChallengeState extends State<WriteChallenge> {
             SizedBox(height: 16.0),
             ElevatedButton(
               onPressed: () async {
-                PickedFile? pickedFile =
-                await ImagePicker().getVideo(source: ImageSource.gallery);
-                setState(() {
-                  files.add(File(pickedFile!.path));
-                });
-                print(files[0]);
+                if (files.length == 0) {
+                  var chooseImage = await ImagePicker()
+                      .pickVideo(source: ImageSource.gallery);
+                  setState(() {
+                    files.add(File(chooseImage!.path));
+                  });
+                }
               },
               child: Text(
                 '갤러리',
                 style: TextStyle(
                     color: (widget.grade == 0 ||
-                        widget.grade == 1 ||
-                        widget.grade == 2 ||
-                        widget.grade == 4 ||
-                        widget.grade == 8)
+                            widget.grade == 1 ||
+                            widget.grade == 2 ||
+                            widget.grade == 4 ||
+                            widget.grade == 8)
                         ? Colors.black
                         : Colors.white),
               ),
@@ -91,10 +126,42 @@ class _WriteChallengeState extends State<WriteChallenge> {
               ),
             ),
             ElevatedButton(
-              onPressed: () {
+              onPressed: () async {
+                var data;
+                var url = "http://${IP_ADDRESS}/test_add_threeboard.php";
+
+                if (files.length != 0) {
+                  String filename = files[0].toString().substring(
+                      files[0].toString().indexOf("image_picker"),
+                      files[0].toString().length - 1);
+
+                  List<int> imgByte = files[0]!.readAsBytesSync();
+                  String img = base64Encode(imgByte);
+
+                  var response = await http.post(Uri.parse(url), body: {
+                    'image': img,
+                    'title': titleController.text.toString(),
+                    'content': contentsController.text.toString(),
+                    'filename': filename.toString(),
+                    'writer': widget.loginID.toString(),
+                  });
+                  data = json.decode(json.encode(response.body));
+                } else {
+                  var response = await http.post(Uri.parse(url), body: {
+                    'image': "no",
+                    'title': titleController.text.toString(),
+                    'content': contentsController.text.toString(),
+                    'filename': "no",
+                    'writer': widget.loginID.toString(),
+                  });
+                  data = json.decode(json.encode(response.body));
+                }
+                print(data.toString());
                 /*
                 insert: 오늘 날짜, loginID, 내용, 영상 저장 (영상은 항상 있어야하는데!!)
                  */
+                /*
+                pop으로 바꾸고 StreamBuilder로 바꿔라! (메인페이지를)
                 Navigator.of(context).pushReplacement(
                   MaterialPageRoute(
                     builder: (BuildContext context) => ChallengeMain(
@@ -104,15 +171,17 @@ class _WriteChallengeState extends State<WriteChallenge> {
                     ),
                   ),
                 );
+
+                 */
               },
               child: Text(
                 '저장',
                 style: TextStyle(
                     color: (widget.grade == 0 ||
-                        widget.grade == 1 ||
-                        widget.grade == 2 ||
-                        widget.grade == 4 ||
-                        widget.grade == 8)
+                            widget.grade == 1 ||
+                            widget.grade == 2 ||
+                            widget.grade == 4 ||
+                            widget.grade == 8)
                         ? Colors.black
                         : Colors.white),
               ),
